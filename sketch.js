d86c63c3315043ce9b45fddd779c33486f41dc75
Glitch (petalite-salty-marlin) <none>
2022-02-22 05:22:45 +0000

    🌾🤞 Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 let d12;
-function preload(){
+function preload() {
   //obtained from here: https://commons.wikimedia.org/wiki/File:Dodecahedron.stl
-  d12 = loadModel("diceModels/Dodecahedron.obj", true);
+  d12 = loadModel(
+    "https://cdn.glitch.global/480fbbe9-66ac-4d90-a58e-7af20abfde25/Dodecahedron.stl?v=1645507143156",
+    true
+  );
 }
 
 function setup() {
   createCanvas(400, 400, WEBGL);
+  //test!
 }
 
 function draw() {
   background(220);
   //translate(0, 0, 200);
-  fill('red');
+  fill("red");
   rotateX(frameCount * 0.01);
   //rotateY(frameCount * 0.01);
   model(d12);
