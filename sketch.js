99f0e729a79ec266744ef506e66d85a2abd1d634
Glitch (petalite-salty-marlin) <none>
2022-02-22 17:17:28 +0000

    🚀⁉️ Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@
 let d12;
 let d20;
 
+//dihedral angle for the different dice
+const D20ANGLE = 138.189685;
+
 //load models for use
 function preload() {
   //obtained from here:
@@ -18,17 +21,36 @@ function setup() {
   //noLoop();
 }
 
-//let i = 0;
+function smoothRotate(angle, speed) {
+  if (!speed){speed=1;}
+  
+}
+
 function draw() {
   //translate(0, 0, 200);
   //fill("red");
+  
+  background(220);
 
-  for (let i = 0; i < 138.189685; i += frameCount * 0.001) {
-    background(220);
-    push();
-    rotateY(frameCount * 0.001);
-    normalMaterial();
-    model(d20);
-    pop();
-  }
+  //draw the d20
+  push();
+  
+  //rotate down half of the exterior dihedral angle to the main face
+  rotateX((360-D20ANGLE)/2);
+  //rotate left by the dihedral angle to the left face
+  rotateY(D20ANGLE);
+  
+  //draw lines for axis
+  push();
+  
+  translate(width/2, height/2, 0);
+  strokeWeight(10);
+  stroke(51);
+  line(0, 400, 0, 0 -400, 0);
+  
+  pop();
+  
+  normalMaterial();
+  model(d20);
+  pop();
 }
