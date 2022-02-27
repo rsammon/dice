5fa8ecdeb5cc3c7a7aa65643b39dbe72cfaaaf05
Glitch (dice-roller-fun) <none>
2022-02-27 01:31:51 +0000

    🐽🦏 Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 //different settings for debugging
 const DEBUG = {
   //have d20 rotate so that all sides can be seen
-  ROT: true,
+  ROT: false,
   //log rot values to console
   ANGLELOG: false,
   //display x, y, and z axes
-  AXIS: true
-}
+  AXIS: true,
+};
 
 //create a dice object template
 class dice {
@@ -42,7 +42,7 @@ function preload() {
 
 function setup() {
   //create canvas w/ webgl so that 3d can be used
-  createCanvas(400, 400, WEBGL);
+  createCanvas(500, 500, WEBGL);
   
 }
 
@@ -50,58 +50,63 @@ function draw() {
   //translate(0, 0, 200);
   //fill("red");
 
-  background(220);
-  
+  background(230);
+ 
   //change angle mode to degreese
   angleMode(DEGREES);
-  
+
   //draw the d20
   push();
 
   //rotate down half of the exterior dihedral angle to the main face
-  rotateX((d20.ANGLE)/2);
+  rotateX(d20.ANGLE / 2);
   //rotate left by the dihedral angle to the left face
 
-  if (DEBUG.ROT){
-  //rotate the d20
-  rotateX(d20.rotX);
-  d20.rotX+=1;
-  rotateY(d20.rotY);
-  d20.rotY+=1;
-  rotateZ(d20.rotZ);
-  d20.rotZ+=1;
+  if (DEBUG.ROT) {
+    //rotate the d20
+    rotateX(d20.rotX);
+    d20.rotX += 1;
+    rotateY(d20.rotY);
+    d20.rotY += 1;
+    rotateZ(d20.rotZ);
+    d20.rotZ += 1;
   }
-  
+
   //log the rot angle measures to the console
-  if (DEBUG.ANGLELOG){
-    console.log(d20.rotX + ', ' + d20.rotY + ', ' + d20.rotZ);
+  if (DEBUG.ANGLELOG) {
+    console.log(d20.rotX + ", " + d20.rotY + ", " + d20.rotZ);
   }
 
-if (DEBUG.AXIS){
-  //draw lines for axes
-  push();
-  strokeWeight(5);
+  if (DEBUG.AXIS) {
+    //draw lines for axes
+    push();
+    strokeWeight(5);
 
-  //z axis
-  stroke("blue");
-  line(0, 400, 0, 0, -400, 0);
+    //z axis
+    stroke("blue");
+    line(0, 400, 0, 0, -400, 0);
 
-  //x axis
-  stroke("red");
-  line(400, 0, 0, -400, 0, 0);
+    //x axis
+    stroke("red");
+    line(400, 0, 0, -400, 0, 0);
 
-  //y axis
-  stroke("green");
-  line(0, 0, 400, 0, 0, -400);
+    //y axis
+    stroke("green");
+    line(0, 0, 400, 0, 0, -400);
 
-  pop();
-}
+    pop();
+  }
 
   //render d20 model
-  //normalMaterial();
-  textureWrap();
+  
+  textureMode(NORMAL);
   texture(d20.txtr);
+  plane(250);
+  debugMode();
+  d20.model.computeFaces();
+  d20.model.computeNormals();
   model(d20.model);
-
+  
   pop();
+  orbitControl();
 }
